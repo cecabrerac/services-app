66d520d9e87fcf9b5d715f85d1530258730b9947
cecabrerac <ccabrera9@hotmail.com>
2023-05-31 11:07:42 -0500

    database password changed

--- src/database/connection.js
+++ src/database/connection.js
@@ -15,10 +15,14 @@ const dbSettings = {
 export async function getConnection() {
   try {
     const pool = await sql.connect(dbSettings);
+    // const result = await pool.request().query("SELECT 1");
+    // console.log(result);
     return pool;
   } catch (error) {
     console.error(error);
   }
 }
+
+getConnection();
 
 export { sql };
