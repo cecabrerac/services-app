4db769dc4b1a88fbdb8c99f4d16a65ad6cd56c1a
cecabrerac <ccabrera9@hotmail.com>
2023-09-19 10:35:11 -0500

    get card route

--- routes/cards.routes.js
+++ routes/cards.routes.js
@@ -4,11 +4,11 @@ const router = express.Router();
 const {
   getAllCards,
   addNewCard,
-  //   getCard,
+  getCard,
 } = require("../controllers/cards.controllers");
 
 router.get("/cards", getAllCards);
 router.post("/cards", addNewCard);
-// router.get("/cards/:id", getCard);
+router.get("/cards/:id", getCard);
 
 module.exports = router;
